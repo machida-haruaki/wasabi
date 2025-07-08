9cb70591414cd01f7ccff9f9f9a1c8746accf89e
machidaharuaki <haruaki.machida-a@techouse.jp>
2025-07-08 09:14:16 +0900

    add: テストだけでなく実行環境でもアロケータを初期化するようにした &　gitの管理下から不要なファイルを除外

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 #![no_main]
 pub mod allocator;
 pub mod graphics;
+pub mod init;
 pub mod qemu;
 pub mod result;
 pub mod serial;
@@ -28,18 +29,6 @@ fn efi_main(
         &mut memory_map,
     );
 
-    // デバッグ: メモリマップの内容を出力
-    let mut sw = serial::SerialPort::new_for_com1();
-    use core::fmt::Write;
-    writeln!(sw, "=== Memory Map Debug ===").unwrap();
-    for e in memory_map.iter() {
-        if e.memory_type() == uefi::EfiMemoryType::CONVENTIONAL_MEMORY {
-            writeln!(sw, "CONVENTIONAL: start={:#x}, pages={}, size={:#x}", 
-                     e.physical_start(), e.number_of_pages(), e.number_of_pages() * 4096).unwrap();
-        }
-    }
-    writeln!(sw, "=== End Memory Map ===").unwrap();
-
-    allocator::ALLOCATOR.init_with_mmap(&memory_map);
+    init::init_basic_runtime(image_handle, efi_system_table);
     run_unit_tests()
 }
